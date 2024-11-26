0e0fcec941e00a440321bd22eceb24d65f104ab2
Nishant Gupta <gupta.nishant08042004@gmail.com>
2024-11-27 01:17:42 +0530

    completed search functionality

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
   
-  const {products} = useContext(ShopContext);
+  const {products , search, showSearch} = useContext(ShopContext);
   const [showFilter,setShowFilter] = useState(false);
   const [filterProducts,setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -24,6 +24,9 @@ const Collection = () => {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+    if(showSearch && search){
+      productsCopy = productsCopy.filter(item=>item.name.toLowerCase().includes(search.toLowerCase()));
+    }
     if(category.length>0){
       productsCopy = productsCopy.filter(item=>category.includes(item.category));
     }
@@ -51,7 +54,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter();
-  },[category])
+  },[category,search,showSearch])
 
   useEffect(()=>{
     sortProduct();
