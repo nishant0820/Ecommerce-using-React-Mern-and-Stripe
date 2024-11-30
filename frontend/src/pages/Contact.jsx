6e375cfec7f9f634db5ceb178a10776e351d2fc0
Nishant Gupta <gupta.nishant08042004@gmail.com>
2024-11-30 21:03:08 +0530

    contact page completed and here frontend is done for ecommerce website

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'CONTACT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+          <p className='text-gray-500'>F-61/2A <br />Bhajanpura, Delhi, India</p>
+          <p className='text-gray-500'>Tel: +91 9899459288 <br />Email: gupta.nishant08042004@gmail.com</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Call Us</button>
+        </div>
+      </div>
+      <NewsletterBox />
+    </div>
   )
 }
 
